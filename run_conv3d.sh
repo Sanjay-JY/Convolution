12d8139ccffa6dfd14aa43e87f7438652c13cbd4
Sanjay-JY <sanjay.jayaseelan@multicorewareinc.com>
2024-08-28 13:11:20 +0530

    Updated folder hierarchy

--- run_conv3d.sh
+++ run_conv3d.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Compile the C++ code
-g++ conv3d.cpp -o conv3d
+g++ ./cpp/conv3d.cpp -o conv3d
 
 # Execute the compiled program
 ./conv3d
 
 # Run the Python script for convolution
-python3 conv3d.py
+python3 ./python/conv3d.py
 
 # Compare the output binary files
-python3 compare_bin.py cpp_conv3d_output.bin py_conv3d_output.bin --dtype int32
+python3 ./python/compare_bin.py cpp_conv3d_output.bin py_conv3d_output.bin --dtype int32
